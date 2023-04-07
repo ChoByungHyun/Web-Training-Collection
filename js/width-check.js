041ce048ad35b8d161f138aa5429363fe7e2be54
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-04-07 22:20:21 +0900

    [Fix] width-check Page 첫 로딩시 width값 표시안되는 버그 수정 - updateWidth로 함수화 하여 수정.

--- js/width-check.js
+++ js/width-check.js
@@ -4,10 +4,24 @@
 //     div.querySelector("p").textContent = "현재 width: " + div.offsetWidth;
 //   }).observe(div);
 // });
-window.addEventListener("resize", function () {
-  const divs = document.querySelectorAll("div");
-  divs.forEach(function (div) {
+
+// window.addEventListener("resize", function () {
+//   const divs = document.querySelectorAll("div");
+//   divs.forEach(function (div) {
+//     div.querySelector("p").textContent =
+//       "Current div width: " + div.offsetWidth;
+//   });
+// });
+const divs = document.querySelectorAll("div");
+divs.forEach((div) => {
+  function updateWidth() {
     div.querySelector("p").textContent =
       "Current div width: " + div.offsetWidth;
+  }
+
+  window.addEventListener("resize", () => {
+    updateWidth();
   });
+
+  updateWidth();
 });
