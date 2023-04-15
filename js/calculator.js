c2895e56800ac5917578c380f875e48b1ae00e64
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-04-16 04:01:23 +0900

    [Feat] 계산기 기능 추가, 개선. textListArea에서 계산되도록 로직 수정. 연산자 클릭 시 색상 변경. 연산자 중복처리 다시 해야함.

--- js/calculator.js
+++ js/calculator.js
@@ -7,7 +7,18 @@ let equalBtn = document.querySelector("#equal");
 
 let lastInput = ""; // 마지막 입력된 문자
 let expressionList = []; // 입력된 수식들을 담을 배열
+let selectedOperator = null;
+let currentSelectedBtn = null;
 
+// 초기화 함수에서 색상 초기화
+function resetColors() {
+  operatorBtns.forEach(function (btn) {
+    btn.style.color = "white";
+    btn.style.backgroundColor = "#f69906";
+  });
+
+  currentSelectedBtn = null;
+}
 numBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
     if (textArea.textContent == "0") {
@@ -23,8 +34,9 @@ numBtns.forEach(function (btn) {
     if (lastInput === "=") {
       textArea.textContent = "";
       textListArea.textContent = "";
-      expressionList = [];
+      // expressionList = [];
     }
+    resetColors();
     lastInput = this.textContent;
     textArea.textContent += this.textContent;
   });
@@ -32,26 +44,30 @@ numBtns.forEach(function (btn) {
 
 equalBtn.addEventListener("click", () => {
   // textArea에 있는 수식을 가져옴
-  var expression = textArea.textContent;
+  // textlistarea에 있는거라 textarea의 값과 연산하자.
+  var expression = textListArea.textContent + textArea.textContent;
+  textListArea.textContent = expression + "=";
 
+  expression = expression.split(" ").join("");
   // "x"를 "*"로 변환
   expression = expression.replace(/x/g, "*");
 
   // "÷"를 "/"로 변환
   expression = expression.replace(/÷/g, "/");
-
   // 수식을 분리하여 계산
   var result = eval(expression);
+  console.log(expression);
 
   // push to expressionList
-  expressionList.push(textArea.textContent + " = ");
+  // expressionList.push(expression + " = ");
 
   // 출력
   textArea.textContent = result;
 
   lastInput = equalBtn.textContent;
-
+  resetColors();
   updateTextList();
+  expressionList = [];
 });
 
 operatorBtns.forEach(function (btn) {
@@ -65,23 +81,21 @@ operatorBtns.forEach(function (btn) {
     }
     if (lastInput !== "" && isNaN(lastInput)) {
       // 마지막 입력된 문자가 연산자인 경우
-      textArea.textContent = textArea.textContent.slice(0, -1); // 마지막 연산자 지우기
+      textListArea.textContent = textListArea.textContent.slice(0, -1); // 마지막 연산자 지우기
       updateTextList();
     }
+    resetColors();
+    // 클릭한 버튼 색상 변경
+    btn.style.backgroundColor = "white";
+    btn.style.color = "#f69906";
+
+    // 현재 선택된 버튼 저장
+    currentSelectedBtn = btn;
 
     lastInput = this.textContent;
-
-    textArea.textContent += this.textContent;
-
-    // 현재까지의 수식과 해당 연산자를 displayText에 저장
-    let currentExp = textArea.textContent.trim();
-    let displayText = "";
-    if (currentExp !== "" && !isNaN(currentExp[currentExp.length - 1])) {
-      displayText = currentExp + " " + this.textContent;
-    } else {
-      displayText = currentExp;
-    }
-    textListArea.textContent = displayText;
+    expressionList.push(textArea.textContent + this.textContent);
+    textListArea.textContent = expressionList.join("");
+    textArea.textContent = "";
   });
 });
 
@@ -92,6 +106,8 @@ extraBtns.forEach(function (btn) {
       lastInput = ""; // 초기화
       expressionList = []; // 수식 리스트 초기화
       textListArea.textContent = "";
+
+      resetColors();
       return;
     }
     if (this.textContent == "+/-") {
@@ -106,6 +122,7 @@ extraBtns.forEach(function (btn) {
       // 마지막 입력된 문자가 연산자인 경우
       textArea.textContent = textArea.textContent.slice(0, -1); // 마지막 연산자 지우기
     }
+
     lastInput = this.textContent;
     textArea.textContent += this.textContent;
   });
@@ -113,8 +130,8 @@ extraBtns.forEach(function (btn) {
 
 function updateTextList() {
   // 추가된 표현식을 표시
-  expressionList.forEach((exp) => {
-    textListArea.textContent = exp;
-    // textListArea.appendChild(listItem)
-  });
+  // expressionList.forEach((exp) => {
+  //   textListArea.textContent = exp;
+  //   // textListArea.appendChild(listItem)
+  // });
 }
