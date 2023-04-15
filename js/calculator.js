18eee4e4bc7c3ed916f5eaa9d30d1ca19d641152
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-04-16 02:25:29 +0900

    [Feat] 계산기 기능 추가, 개선. 상단 수식창 추가, 새로운 숫자입력시 초기화, 연산자 클릭 시 상단 수식창에 표시.

--- js/calculator.js
+++ js/calculator.js
@@ -3,8 +3,10 @@ let extraBtns = document.querySelectorAll(".extra-operator");
 let operatorBtns = document.querySelectorAll(".operator");
 let textArea = document.querySelector(".text-area");
 let textListArea = document.querySelector(".text-list-area");
+let equalBtn = document.querySelector("#equal");
 
 let lastInput = ""; // 마지막 입력된 문자
+let expressionList = []; // 입력된 수식들을 담을 배열
 
 numBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
@@ -18,42 +20,68 @@ numBtns.forEach(function (btn) {
     if (this.textContent === "." && textArea.textContent.includes(".")) {
       return;
     }
+    if (lastInput === "=") {
+      textArea.textContent = "";
+      textListArea.textContent = "";
+      expressionList = [];
+    }
     lastInput = this.textContent;
     textArea.textContent += this.textContent;
   });
 });
 
+equalBtn.addEventListener("click", () => {
+  // textArea에 있는 수식을 가져옴
+  var expression = textArea.textContent;
+
+  // "x"를 "*"로 변환
+  expression = expression.replace(/x/g, "*");
+
+  // "÷"를 "/"로 변환
+  expression = expression.replace(/÷/g, "/");
+
+  // 수식을 분리하여 계산
+  var result = eval(expression);
+
+  // push to expressionList
+  expressionList.push(textArea.textContent + " = ");
+
+  // 출력
+  textArea.textContent = result;
+
+  lastInput = equalBtn.textContent;
+
+  updateTextList();
+});
+
 operatorBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
-    if (this.textContent == "=") {
-      // textArea에 있는 수식을 가져옴
-      var expression = textArea.textContent;
+    if (this.textContent === "=") {
+      return;
+    }
 
-      // "x"를 "*"로 변환
-      expression = expression.replace(/x/g, "*");
-
-      // "÷"를 "/"로 변환
-      expression = expression.replace(/÷/g, "/");
-
-      // 수식을 분리하여 계산
-      var result = eval(expression);
-
-      // 출력
-      textArea.textContent = result;
-
+    if (textArea.textContent === "0") {
       return;
     }
     if (lastInput !== "" && isNaN(lastInput)) {
       // 마지막 입력된 문자가 연산자인 경우
       textArea.textContent = textArea.textContent.slice(0, -1); // 마지막 연산자 지우기
-    }
-    if (textArea.textContent === "0") {
-      return;
+      updateTextList();
     }
 
     lastInput = this.textContent;
 
     textArea.textContent += this.textContent;
+
+    // 현재까지의 수식과 해당 연산자를 displayText에 저장
+    let currentExp = textArea.textContent.trim();
+    let displayText = "";
+    if (currentExp !== "" && !isNaN(currentExp[currentExp.length - 1])) {
+      displayText = currentExp + " " + this.textContent;
+    } else {
+      displayText = currentExp;
+    }
+    textListArea.textContent = displayText;
   });
 });
 
@@ -62,6 +90,8 @@ extraBtns.forEach(function (btn) {
     if (this.textContent == "AC") {
       textArea.textContent = 0;
       lastInput = ""; // 초기화
+      expressionList = []; // 수식 리스트 초기화
+      textListArea.textContent = "";
       return;
     }
     if (this.textContent == "+/-") {
@@ -80,3 +110,11 @@ extraBtns.forEach(function (btn) {
     textArea.textContent += this.textContent;
   });
 });
+
+function updateTextList() {
+  // 추가된 표현식을 표시
+  expressionList.forEach((exp) => {
+    textListArea.textContent = exp;
+    // textListArea.appendChild(listItem)
+  });
+}
