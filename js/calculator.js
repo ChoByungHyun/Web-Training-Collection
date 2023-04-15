e9cfdfd3a4429904e25aa54ffb848c6d1591e4cb
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-04-15 23:56:50 +0900

    [Feat] 계산기 기능 추가, 개선. dot(.)중복불가처리, 연산자 중복불가처리.

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,24 @@
 let numBtns = document.querySelectorAll(".num");
-let ExtraBtns = document.querySelectorAll(".extra-operator");
+let extraBtns = document.querySelectorAll(".extra-operator");
 let operatorBtns = document.querySelectorAll(".operator");
 let textArea = document.querySelector(".text-area");
+let textListArea = document.querySelector(".text-list-area");
+
+let lastInput = ""; // 마지막 입력된 문자
 
 numBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
     if (textArea.textContent == "0") {
       textArea.textContent = "";
+      if (this.textContent === ".") {
+        textArea.textContent = "0.";
+        return;
+      }
     }
+    if (this.textContent === "." && textArea.textContent.includes(".")) {
+      return;
+    }
+    lastInput = this.textContent;
     textArea.textContent += this.textContent;
   });
 });
@@ -29,16 +40,28 @@ operatorBtns.forEach(function (btn) {
 
       // 출력
       textArea.textContent = result;
+
       return;
     }
+    if (lastInput !== "" && isNaN(lastInput)) {
+      // 마지막 입력된 문자가 연산자인 경우
+      textArea.textContent = textArea.textContent.slice(0, -1); // 마지막 연산자 지우기
+    }
+    if (textArea.textContent === "0") {
+      return;
+    }
+
+    lastInput = this.textContent;
+
     textArea.textContent += this.textContent;
   });
 });
 
-ExtraBtns.forEach(function (btn) {
+extraBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
     if (this.textContent == "AC") {
       textArea.textContent = 0;
+      lastInput = ""; // 초기화
       return;
     }
     if (this.textContent == "+/-") {
@@ -49,6 +72,11 @@ ExtraBtns.forEach(function (btn) {
       textArea.textContent = textArea.textContent * 0.01;
       return;
     }
+    if (lastInput !== "" && isNaN(lastInput)) {
+      // 마지막 입력된 문자가 연산자인 경우
+      textArea.textContent = textArea.textContent.slice(0, -1); // 마지막 연산자 지우기
+    }
+    lastInput = this.textContent;
     textArea.textContent += this.textContent;
   });
 });
