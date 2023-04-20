33b4adfcbbeae713a267a7a484269d49e57131f0
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-04-20 20:20:29 +0900

    [Feat] MainPage Update, NotePad local connect

--- js/notepad.js
+++ js/notepad.js
@@ -4,17 +4,6 @@ const btnSubmit = document.querySelector(".black-btn");
 const listDiaryTitle = document.querySelector(".article-title");
 const listDiaryContent = document.querySelector(".article-content");
 
-const memoList = [];
-const memo2 = {};
-memo2["one"] = "메모1";
-memo2["two"] = "메모2";
-localStorage.setItem("메모2", JSON.stringify(memo2));
-
-console.log(localStorage.getItem("메모2"));
-console.log(JSON.parse(localStorage.getItem("메모2")));
-
-// allMemo = allMemo ?? [];
-
 btnSubmit.addEventListener("click", () => {
   listDiaryTitle.textContent = diaryTitle.value;
   listDiaryContent.textContent = diaryContent.value;
@@ -25,3 +14,80 @@ btnSubmit.addEventListener("click", () => {
   localStorage.setItem("리스트", JSON.stringify(allMemo));
   console.log(JSON.parse(localStorage.getItem("리스트")));
 });
+
+function render() {
+  const display = document.getElementById("display");
+  display.innerHTML = "";
+
+  for (const item of allMemo) {
+    const saveTitle = document.createElement("h3");
+    const saveContent = document.createElement("p");
+    const saveId = document.createElement("p");
+    const deleteMemoBtn = document.createElement("button");
+
+    saveTitle.textContent = item.title;
+    saveContent.textContent = item.content;
+    saveId.textContent = item.len + 1;
+    deleteMemoBtn.textContent = "삭제";
+    deleteMemoBtn.setAttribute("id", item.len);
+    deleteMemoBtn.setAttribute("onclick", "remove()");
+
+    display.appendChild(saveId);
+    display.appendChild(saveTitle);
+    display.appendChild(saveContent);
+    display.appendChild(deleteMemoBtn);
+  }
+}
+
+//sample code
+// let allMemo = JSON.parse(localStorage.getItem("allMemo"));
+// allMemo = allMemo ?? [];
+// render();
+
+// function saveNote() {
+//   const title = document.getElementById("title").value;
+//   const content = document.getElementById("content").value;
+
+//   allMemo.push({ title, content, len: allMemo.length });
+
+//   localStorage.setItem("allMemo", JSON.stringify(allMemo));
+//   render();
+// }
+
+// function render() {
+//   const display = document.getElementById("display");
+//   display.innerHTML = "";
+
+//   for (const item of allMemo) {
+//     const saveTitle = document.createElement("h2");
+//     const saveContent = document.createElement("p");
+//     const saveId = document.createElement("p");
+//     const deleteMemoBtn = document.createElement("button");
+
+//     saveTitle.textContent = item.title;
+//     saveContent.textContent = item.content;
+//     saveId.textContent = item.len + 1;
+//     deleteMemoBtn.textContent = "삭제";
+//     deleteMemoBtn.setAttribute("id", item.len);
+//     deleteMemoBtn.setAttribute("onclick", "remove()");
+
+//     display.appendChild(saveId);
+//     display.appendChild(saveTitle);
+//     display.appendChild(saveContent);
+//     display.appendChild(deleteMemoBtn);
+//   }
+// }
+
+// function remove() {
+//   // console.log(event.srcElement.id);
+//   // console.log(allMemo);
+//   const idx = allMemo.find((item) => item.len == event.srcElement.id);
+//   if (idx) {
+//     allMemo.splice(
+//       allMemo.findIndex((item) => item.len == idx.len),
+//       1
+//     );
+//   }
+//   localStorage.setItem("allMemo", JSON.stringify(allMemo));
+//   render();
+// }
