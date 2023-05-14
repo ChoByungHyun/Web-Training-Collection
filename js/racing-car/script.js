053a9eb810caee23cbaf153bf115ccb7dd18083e
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-05-15 00:40:56 +0900

    [Feat] 자동차 리스트 카운트에 맞게 출력.

--- js/racing-car/script.js
+++ js/racing-car/script.js
@@ -3,41 +3,72 @@ const btnCarName = document.querySelector("#car-names-submit");
 const racingCountInput = document.querySelector("#racing-count-input");
 const btnRacingCount = document.querySelector("#racing-count-submit");
 const racingWinner = document.querySelector("#racing-winners");
+
 const result = document.querySelector(".result");
+
+const resultDiv = document.createElement("div");
+resultDiv.className = "result-div";
+result.after(resultDiv);
 
 function Car(name) {
   this.name = name;
 }
 
-let carNames = [];
+let carNames = "";
 
 btnCarName.addEventListener("click", () => {
   console.log(carNameInput.value);
-  carNames.push(new Car(carNameInput.value));
-  carNameInput.value = "";
 
   console.log(carNames);
+  racingCarName();
 });
 
-let racingCount = [];
+function racingCarNameSplit() {
+  carNames = carNames.name.split(",");
+}
+const carNameList = document.createElement("div");
+
+function racingCarName() {
+  carNames = new Car(carNameInput.value);
+  console.log(carNames);
+  racingCarNameSplit();
+  console.log(carNames);
+
+  resultDiv.appendChild(carNameList);
+  carNameList.innerHTML = `<div>참여한 자동차들은 ${carNames} 입니다 !</div>`;
+  carNameInput.value = "";
+}
+
+let racingCount = "";
 
 btnRacingCount.addEventListener("click", () => {
   racingStart();
-
-  // result.innerHTML = `<div>The winner is ${carNames[racingWinnerIndex].name} !</div>`;
 });
 
 function racingStart() {
   if (racingCountInput.value) {
-    racingCount.push(racingCountInput.value);
+    racingCount = racingCountInput.value;
     racingCountInput.value = "";
+  } else {
+    alert("숫자를 입력해주세요!");
+    return;
   }
+  racingProcess(parseInt(racingCount));
   console.log(racingCount);
-  const test = document.createElement("div");
-  test.innerHTML = `<div>The winner is ${carNames[racingWinnerIndex].name} !</div>`;
-  result.after(test);
+  // resultDiv.innerHTML = `<div>The winner is ${carNames} !</div>`;
+}
+function racingProcess(count) {
+  let racingCar = [];
+
+  for (let i = 0; i < count; i++) {
+    const carListSection = document.createElement("div");
+    carNameList.appendChild(carListSection);
+    carListSection.className = "car-list-section";
+    for (let j = 0; j < carNames.length; j++) {
+      carListSection.innerHTML += `<div>${carNames[j]}</div>`;
+    }
+  }
+  console.log(racingCar);
 }
 
 let racingWinnerIndex = 0;
-
-// setInterval(racing, 1000);
